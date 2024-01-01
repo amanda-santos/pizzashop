64d002fd091b18df8ac4e694b84720c80991d241
amanda-santos <amandasf0200@gmail.com>
2024-01-01 19:10:34 -0300

    chore: ➕ setup shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+import './global.css'
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
