5863e595d2d9984af39ad7eda8108cd480283436
amanda-santos <amandasf0200@gmail.com>
2024-01-03 19:35:39 -0300

    feat: ✨ add account menu and theme toggle in header

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <Toaster richColors />
+        <Toaster richColors />
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
